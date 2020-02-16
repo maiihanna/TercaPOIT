2a2000337e285dd82d9ad3df0fffda86cf7c0cbd
Viola-25 <marquesenzo@hotmail.com>
2020-02-16 20:02:09 +0000

    Comentei o código Blink com a função de cada parte do código

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,7 +1,10 @@
-void setup (){
-    pinMode(10, OUTPUT);
+void setup (){ //Configurações iniciais do código
+    pinMode(10, OUTPUT); //Porta 10 é salva como OUTPUT, ou seja, saída de energia
 }
-
-void loop (){
-
+//ÚTIL:Delay de 1500 milisegundos == 1,5 segundo
+void loop (){ //Código que ira se repetir constantemente
+//Acender o LED
+//Dar um delay
+//Apagar o LED
+//Dar um delay
 }
