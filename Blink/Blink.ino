838f093b14e1b83e0efd61eb15fc8a11a1adfc94
maiihanna <60942164+maiihanna@users.noreply.github.com>
2020-02-17 17:21:41 -0300

    Mai definiu a porta do led
    pinMode(10,OUTPUT);

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,5 +1,5 @@
 void setup (){ //Configurações iniciais do código
-    pinMode(10, OUTPUT); //Porta 10 é salva como OUTPUT, ou seja, saída de energia
+    pinMode(10,OUTPUT);//Porta 10 é salva como OUTPUT, ou seja, saída de energia
 }
 //ÚTIL:Delay de 1500 milisegundos == 1,5 segundo
 void loop (){ //Código que ira se repetir constantemente
